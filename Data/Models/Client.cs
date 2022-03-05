bcb42da835e986566291dd2ff1697f2cc1392c64
antoaneta mandazhieva <toni_mandazhieva@abv.bg>
2022-03-05 12:18:13 +0200

    Created class Data.Client

---
+++ Data/Models/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Nationality { get; set; }
+        public string PassportNumber { get; set; }
+    }
+}
