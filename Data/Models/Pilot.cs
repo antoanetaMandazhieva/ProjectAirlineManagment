6bf0e47012cd1a6587cf9cfa50768f483b3be249
Teodora <tedigocheva@abv.bg>
2022-03-06 11:30:18 +0200

    Created class Data.Pilot

--- Data/Models/Pilot.cs
+++ Data/Models/Pilot.cs
@@ -8,5 +8,8 @@ namespace Data.Models
 {
     public class Pilot
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int FlightId { get; set; }
     }
 }
