f03ec108a06085c9977ae80b60ffee6a6ae1ef91
antoaneta mandazhieva <toni_mandazhieva@abv.bg>
2022-03-12 13:54:12 +0200

    edited pilot migration

---
+++ Data/Migrations/202203121153157_editedPilot.cs
@@ -0,0 +1,22 @@
+﻿namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class editedPilot : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Pilots", "PhoneNumber", c => c.String());
+            AddColumn("dbo.Pilots", "Age", c => c.Int(nullable: false));
+            AddColumn("dbo.Pilots", "TypePilot", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Pilots", "TypePilot");
+            DropColumn("dbo.Pilots", "Age");
+            DropColumn("dbo.Pilots", "PhoneNumber");
+        }
+    }
+}
--- Data/Models/Pilot.cs
+++ Data/Models/Pilot.cs
@@ -10,6 +10,9 @@ namespace Data.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Age { get; set; }
+        public string TypePilot { get; set; }
         public int FlightId { get; set; }
     }
 }
