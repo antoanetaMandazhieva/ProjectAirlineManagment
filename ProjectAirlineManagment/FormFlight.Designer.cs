e64cc7dba7035be14100aa4904793ecd696a4a17
antoaneta mandazhieva <toni_mandazhieva@abv.bg>
2022-03-12 11:28:00 +0200

    FlightForm

--- ProjectAirlineManagment/FormFlight.Designer.cs
+++ ProjectAirlineManagment/FormFlight.Designer.cs
@@ -29,12 +29,203 @@ namespace ProjectAirlineManagment
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.labelDestination = new System.Windows.Forms.Label();
+            this.labelSeat = new System.Windows.Forms.Label();
+            this.dateTimePickerDate = new System.Windows.Forms.DateTimePicker();
+            this.comboBoxDestination = new System.Windows.Forms.ComboBox();
+            this.textBoxNumberOfSeats = new System.Windows.Forms.TextBox();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.dataGridViewFlight = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFlight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Lucida Bright", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelTitle.Location = new System.Drawing.Point(26, 23);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(91, 27);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "Flights";
+            this.labelTitle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDate.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelDate.Location = new System.Drawing.Point(44, 91);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(46, 18);
+            this.labelDate.TabIndex = 1;
+            this.labelDate.Text = "Date";
+            // 
+            // labelDestination
+            // 
+            this.labelDestination.AutoSize = true;
+            this.labelDestination.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDestination.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelDestination.Location = new System.Drawing.Point(44, 138);
+            this.labelDestination.Name = "labelDestination";
+            this.labelDestination.Size = new System.Drawing.Size(98, 18);
+            this.labelDestination.TabIndex = 2;
+            this.labelDestination.Text = "Destination";
+            // 
+            // labelSeat
+            // 
+            this.labelSeat.AutoSize = true;
+            this.labelSeat.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSeat.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelSeat.Location = new System.Drawing.Point(44, 181);
+            this.labelSeat.Name = "labelSeat";
+            this.labelSeat.Size = new System.Drawing.Size(144, 18);
+            this.labelSeat.TabIndex = 3;
+            this.labelSeat.Text = "Number of Seats";
+            // 
+            // dateTimePickerDate
+            // 
+            this.dateTimePickerDate.CalendarFont = new System.Drawing.Font("Lucida Bright", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePickerDate.Font = new System.Drawing.Font("Lucida Bright", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePickerDate.Location = new System.Drawing.Point(201, 91);
+            this.dateTimePickerDate.Name = "dateTimePickerDate";
+            this.dateTimePickerDate.Size = new System.Drawing.Size(183, 20);
+            this.dateTimePickerDate.TabIndex = 4;
+            // 
+            // comboBoxDestination
+            // 
+            this.comboBoxDestination.FormattingEnabled = true;
+            this.comboBoxDestination.Items.AddRange(new object[] {
+            "Tirana, Albania",
+            "Sarajevo, Bosnia and Herzegovina",
+            "Sofia, Bulgaria",
+            "Zagreb, Croatia",
+            "Athens, Greece",
+            "Pristina, Kosovo",
+            "Podgorica, Montenegro",
+            "Skopje, North Macedonia",
+            "Bucharest, Romania",
+            "Belgrade, Sebia",
+            "Ljubljana, Slovenia",
+            "Ankara, Turkey"});
+            this.comboBoxDestination.Location = new System.Drawing.Point(201, 138);
+            this.comboBoxDestination.Name = "comboBoxDestination";
+            this.comboBoxDestination.Size = new System.Drawing.Size(183, 21);
+            this.comboBoxDestination.TabIndex = 5;
+            // 
+            // textBoxNumberOfSeats
+            // 
+            this.textBoxNumberOfSeats.Location = new System.Drawing.Point(201, 181);
+            this.textBoxNumberOfSeats.Name = "textBoxNumberOfSeats";
+            this.textBoxNumberOfSeats.Size = new System.Drawing.Size(67, 20);
+            this.textBoxNumberOfSeats.TabIndex = 6;
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(170)))), ((int)(((byte)(180)))));
+            this.buttonInsert.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonInsert.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonInsert.Location = new System.Drawing.Point(551, 233);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(77, 28);
+            this.buttonInsert.TabIndex = 7;
+            this.buttonInsert.Text = "Insert";
+            this.buttonInsert.UseVisualStyleBackColor = false;
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(170)))), ((int)(((byte)(180)))));
+            this.buttonUpdate.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUpdate.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonUpdate.Location = new System.Drawing.Point(551, 293);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(77, 28);
+            this.buttonUpdate.TabIndex = 8;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = false;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(170)))), ((int)(((byte)(180)))));
+            this.buttonDelete.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDelete.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonDelete.Location = new System.Drawing.Point(551, 356);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(77, 28);
+            this.buttonDelete.TabIndex = 9;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = false;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(170)))), ((int)(((byte)(180)))));
+            this.buttonSave.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSave.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonSave.Location = new System.Drawing.Point(551, 419);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(77, 28);
+            this.buttonSave.TabIndex = 10;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = false;
+            // 
+            // dataGridViewFlight
+            // 
+            this.dataGridViewFlight.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(170)))), ((int)(((byte)(180)))));
+            this.dataGridViewFlight.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridViewFlight.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewFlight.Location = new System.Drawing.Point(31, 233);
+            this.dataGridViewFlight.Name = "dataGridViewFlight";
+            this.dataGridViewFlight.Size = new System.Drawing.Size(484, 214);
+            this.dataGridViewFlight.TabIndex = 11;
+            // 
+            // FormFlight
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(661, 473);
+            this.Controls.Add(this.dataGridViewFlight);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonInsert);
+            this.Controls.Add(this.textBoxNumberOfSeats);
+            this.Controls.Add(this.comboBoxDestination);
+            this.Controls.Add(this.dateTimePickerDate);
+            this.Controls.Add(this.labelSeat);
+            this.Controls.Add(this.labelDestination);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.labelTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormFlight";
             this.Text = "FormFlight";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FormFlight_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFlight)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Label labelDestination;
+        private System.Windows.Forms.Label labelSeat;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDate;
+        private System.Windows.Forms.ComboBox comboBoxDestination;
+        private System.Windows.Forms.TextBox textBoxNumberOfSeats;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.DataGridView dataGridViewFlight;
     }
 }
--- ProjectAirlineManagment/FormTickets.Designer.cs
+++ ProjectAirlineManagment/FormTickets.Designer.cs
@@ -29,10 +29,17 @@ namespace ProjectAirlineManagment
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormTickets
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FormTickets";
             this.Text = "FormTickets";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
