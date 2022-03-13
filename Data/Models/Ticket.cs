5429bd0de58cb9f857b7e8ca9ad60bc46b99840f
Teodora <tedigocheva@abv.bg>
2022-03-13 12:25:28 +0200

    Updated XML for Data.Models.Ticket

--- Data/Models/Ticket.cs
+++ Data/Models/Ticket.cs
@@ -14,37 +14,31 @@ namespace Data.Models
     {
         /// <summary>
         /// Create an object and properties.
-        /// <param name="Id">Name for variable id input.</param>
         /// </summary>
         public int Id { get; set; }
 
         /// <summary>
         /// Create an object and properties.
-        /// <param name="ClientId">Name for variable client id input.</param>
         /// </summary>
         public int ClientId { get; set; }
 
         /// <summary>
         /// Create an object and properties.
-        /// <param name="FlightId">Name for variable flight id input.</param>
         /// </summary>
         public int FlightId { get; set; }
 
         /// <summary>
         /// Create an object and properties.
-        /// <param name="Price">Name for variable price input.</param>
         /// </summary>
         public decimal Price { get; set; }
 
         /// <summary>
         /// Create an object and properties.
-        /// <param name="Seat">Nmae for variable seat input.</param>
         /// </summary>
         public string Seat { get; set; }
 
         /// <summary>
         /// Create an object and properties.
-        /// <param name="TypeTicket">Name for variable type of ticket input.</param>
         /// </summary>
         public string TypeTicket { get; set; }
     }
