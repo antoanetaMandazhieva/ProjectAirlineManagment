77633ec6cb8012140a0a68d41721caaa0e1119bc
Teodora <tedigocheva@abv.bg>
2022-03-13 12:23:35 +0200

    Update XML forData.Models.Pilot

--- Data/Models/Pilot.cs
+++ Data/Models/Pilot.cs
@@ -14,37 +14,31 @@ namespace Data.Models
     {
         /// <summary>
         /// Create an object and properties.
-        /// <param name="Id">Name variable for id input.</param>
-        /// </summary>
+        /// /// </summary>
         public int Id { get; set; }
        
         /// <summary>
         /// Create an object and properties.
-        /// <param name="Name">Name variable for name input</param>
         /// </summary>
         public string Name { get; set; }
 
         /// <summary>
-        /// Create an object an properties
-        /// <param name="PhoneNumer">Name for variable phone number input.</param>
+        /// Create an object an properties.
         /// </summary>
         public string PhoneNumber { get; set; }
 
         /// <summary>
         /// Create an object and properties.
-        /// <param name="Age">Name for variable age input.</param>
         /// </summary>
         public int Age { get; set; }
 
         /// <summary>
         /// Create an object and properties.
-        /// <param name="TypePilot">Name for variable type of pilot input.</param>
         /// </summary>
         public string TypePilot { get; set; }
 
         /// <summary>
         /// Create an object and properties.
-        /// <param name="FlightId">Name for variable flight id input.</param>
         /// </summary>
         public int FlightId { get; set; }
     }
