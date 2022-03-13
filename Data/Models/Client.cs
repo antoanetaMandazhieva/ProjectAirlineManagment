12ee0ea22215a1486e8156d88cf5a6c9ddda5439
Teodora <tedigocheva@abv.bg>
2022-03-13 11:32:21 +0200

    XML for Data.Models.Client

--- Data/Models/Client.cs
+++ Data/Models/Client.cs
@@ -6,12 +6,40 @@ using System.Threading.Tasks;
 
 namespace Data.Models
 {
+    /// <summary>
+    /// Create class Client.
+    /// This class contains objects with their properties.
+    /// </summary>
     public class Client
     {
+        /// <summary>
+        /// Create an object with properties.
+        /// <param name="Id">Name for the variable for the id input.</param>
+        /// </summary>
         public int Id { get; set; }
+       
+        /// <summary>
+        /// Create an object with properties.
+        /// <param name="Name">Name for variable for the name input.</param>
+        /// </summary>
         public string Name { get; set; }
+      
+        /// <summary>
+        /// Create an object with properties.
+        /// <param name="PhoneNumber">Name for variable for the phone number input.</param>
+        /// </summary>
         public string PhoneNumber { get; set; }
+       
+        /// <summary>
+        /// Create an object with properties.
+        /// <param name="Nationality">Name for variable for the nationality input.</param>
+        /// </summary>
         public string Nationality { get; set; }
+        
+        /// <summary>
+        /// Create an object with properties.
+        /// <param name="PassportNumber">Name for variable passport number input.</param>
+        /// </summary>
         public string PassportNumber { get; set; }
     }
 }
