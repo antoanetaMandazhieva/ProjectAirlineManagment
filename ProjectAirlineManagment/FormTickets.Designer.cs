280658d5c9b890d25d6cbd8f9aa2a9084c4ab313
Teodora <tedigocheva@abv.bg>
2022-03-19 16:37:52 +0200

    updates FormTickets

--- ProjectAirlineManagment/FormTickets.Designer.cs
+++ ProjectAirlineManagment/FormTickets.Designer.cs
@@ -403,7 +403,7 @@ namespace ProjectAirlineManagment
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.LightGray;
+            this.BackColor = System.Drawing.Color.DarkGray;
             this.ClientSize = new System.Drawing.Size(846, 474);
             this.Controls.Add(this.rjRadioButtonRoundTrip);
             this.Controls.Add(this.rjRadioButtonOneWay);
